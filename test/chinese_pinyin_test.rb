af3c709ffb61a5fd1f5ec01090fa0c21c54a0837
P.S.V.R <pmq2001@gmail.com>
2011-08-21 15:48:30 +0800

    In order to make tests pass under Ruby 1.9

--- test/chinese_pinyin_test.rb
+++ test/chinese_pinyin_test.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'test_helper'
 
 class PinyinTest < Test::Unit::TestCase
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'rubygems'
 require 'test/unit'
 require File.join(File.dirname(__FILE__) + '/../lib/chinese_pinyin')
