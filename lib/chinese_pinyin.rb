f4df5b3241c10de766eb8589f88a0e3c1aafdf86
Richard Huang <flyerhzm@gmail.com>
2011-10-08 21:22:38 +0800

    use Words.dat to solve polyphone issue

--- lib/chinese_pinyin.rb
+++ lib/chinese_pinyin.rb
@@ -2,7 +2,7 @@
 $KCODE = 'u' if RUBY_VERSION !~ /1\.9/
 
 class Pinyin
-  
+
   class <<self
     attr_accessor :table
 
@@ -17,7 +17,23 @@ class Pinyin
       end
     end
 
+    def init_word_table
+      return if @words_table
+      @words_table = {}
+      if ENV["WORDS_FILE"]
+        open(ENV["WORDS_FILE"]) do |file|
+          while line = file.gets
+            key, value = line.sub("\n", "").split('|', 2)
+            @words_table[key] = value
+          end
+        end
+      end
+    end
+
     def translate(chars, splitter = ' ')
+      init_word_table
+      return @words_table[chars].gsub(' ', splitter) if @words_table[chars]
+
       init_table
       results = []
       is_english = false
--- test/chinese_pinyin_test.rb
+++ test/chinese_pinyin_test.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 require 'test_helper'
 
+ENV["WORDS_FILE"] = File.dirname(__FILE__) + '/Words.dat'
+
 class PinyinTest < Test::Unit::TestCase
   def test_t
     assert_equal("zhong guo", Pinyin.t('中国'))
@@ -9,5 +11,7 @@ class PinyinTest < Test::Unit::TestCase
     assert_equal("huangzhimin", Pinyin.t('黄志敏', ''))
 
     assert_equal("zhong guo english ri", Pinyin.t('中国english日'))
+
+    assert_equal("guang-zhou", Pinyin.t('广州', '-'))
   end
 end
